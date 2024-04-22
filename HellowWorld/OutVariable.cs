1d205e9a2ad3b6589747484db85b1a0649a42cf4
admin <admin@DESKTOP-IF37CL9>
2024-04-22 15:20:22 +0900

    초기 커밋.

---
+++ HellowWorld/OutVariable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+
+class OutVariable
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("값을 입력해주세요. ==> ");
+        var xx = Console.ReadLine();
+        if (int.TryParse(xx, out int value))
+        {
+            Console.WriteLine(value);
+        }
+    }
+}
